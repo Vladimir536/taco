97fbfd00425564e9b12396b9f06283645f1ec177
Vladimir536 <fantasia1996@mail.ru>
2015-10-30 12:37:38 +0900

    Login and MainWindow added

---
+++ src/MainWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by User07 on 23.10.15.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super();
+
+       this.setVisible(true);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
